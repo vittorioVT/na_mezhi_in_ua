a9ea881973814333bff3094c0b2b6059cb166fd4
Viktor Tiutiun <vvtiutiun@googlemail.com>
2020-04-28 20:12:18 +0300

    Created mainLayout, homePage, postPage components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainLayoutComponent } from './shared/components/main-layout/main-layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: MainLayoutComponent, children: [
+
+      ]
+  }
+];
   
     //path: '', component: MainLayoutComponent, children: [
     //  { path: '', redirectTo: '/', pathMatch: 'full' },
