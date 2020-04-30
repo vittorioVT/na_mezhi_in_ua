6b6f6f90d730abb46939277189b02b8fb4a6e6dd
Viktor Tiutiun <vvtiutiun@googlemail.com>
2020-04-30 13:13:39 +0300

    Create edit page for admin panel

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -5,9 +5,10 @@ import { AdminLayoutComponent } from './shared/components/admin-layout/admin-lay
 import { LoginPageComponent } from './login-page/login-page.component';
 import { DashboardPageComponent } from './dashboard-page/dashboard-page.component';
 import { CreatePageComponent } from './create-page/create-page.component';
+import { EditPageComponent } from './edit-page/edit-page.component';
 
 @NgModule({
-  declarations: [AdminLayoutComponent, LoginPageComponent, DashboardPageComponent, CreatePageComponent],
+  declarations: [AdminLayoutComponent, LoginPageComponent, DashboardPageComponent, CreatePageComponent, EditPageComponent],
   imports: [
     CommonModule,
     RouterModule.forChild([
