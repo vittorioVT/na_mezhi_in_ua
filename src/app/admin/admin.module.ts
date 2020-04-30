f660215f1689d15989f6b98afddd8082d920fed6
Viktor Tiutiun <vvtiutiun@googlemail.com>
2020-04-30 13:12:12 +0300

    Create create-page for admin panel

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -4,9 +4,10 @@ import { RouterModule } from '@angular/router';
 import { AdminLayoutComponent } from './shared/components/admin-layout/admin-layout.component';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { DashboardPageComponent } from './dashboard-page/dashboard-page.component';
+import { CreatePageComponent } from './create-page/create-page.component';
 
 @NgModule({
-  declarations: [AdminLayoutComponent, LoginPageComponent, DashboardPageComponent],
+  declarations: [AdminLayoutComponent, LoginPageComponent, DashboardPageComponent, CreatePageComponent],
   imports: [
     CommonModule,
     RouterModule.forChild([
