a8b439e9fc782448cb30d909288b9f81600edd30
Viktor Tiutiun <vvtiutiun@googlemail.com>
2020-05-02 15:42:05 +0300

    Created AuthService and Shared module

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+
+import { AuthService } from './shared/services/auth.service';
+
 import { AdminLayoutComponent } from './shared/components/admin-layout/admin-layout.component';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { DashboardPageComponent } from './dashboard-page/dashboard-page.component';
 import { CreatePageComponent } from './create-page/create-page.component';
 import { EditPageComponent } from './edit-page/edit-page.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+
 
 @NgModule({
   declarations: [AdminLayoutComponent, LoginPageComponent, DashboardPageComponent, CreatePageComponent, EditPageComponent],
@@ -27,9 +33,9 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     ]),
   ],
   exports: [RouterModule],
-  providers: []
+  providers: [AuthService]
   })
 export class AdminModule {
-
+  constructor(private http: HttpClient) { }
 
 }
