9466e854d512f40e7a1976c5e9ef8e8fa14fb52c
Viktor Tiutiun <vvtiutiun@googlemail.com>
2020-05-02 17:47:27 +0300

    Injected auth service and router in login component for authorization

--- src/app/admin/shared/services/auth.service.ts
+++ src/app/admin/shared/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { User } from '../interfaces';
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class AuthService {
@@ -10,8 +11,8 @@ export class AuthService {
     return '';
   }
 
-  login(user: User) {
-    this.http.post('', user);
+  login(user: User): Observable<any> {
+    return this.http.post('', user);
   }
 
   isAuthenticated(): boolean {
