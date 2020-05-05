601854a9cdceb027fda234c37ddfb1184a059315
Viktor Tiutiun <vvtiutiun@googlemail.com>
2020-05-05 12:57:21 +0300

    Created template for creating form single post of blog

--- src/app/admin/create-page/create-page.component.ts
+++ src/app/admin/create-page/create-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create-page',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreatePageComponent implements OnInit {
 
+  form: FormGroup;
+
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.form = new FormGroup({
+      title: new FormControl(null, Validators.required),
+      text: new FormControl(null, Validators.required),
+      author: new FormControl(null, Validators.required),
+    });
   }
 
+  submit() {
+    if (this.form.invalid) {
+      return;
+    }
+  }
 }
