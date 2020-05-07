a82d40f5792c01b8db09a781875440ef3d4f7416
Viktor Tiutiun <vvtiutiun@googlemail.com>
2020-05-07 22:08:38 +0300

    Created html-template for main page of admin

--- src/app/admin/dashboard-page/dashboard-page.component.ts
+++ src/app/admin/dashboard-page/dashboard-page.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { PostService } from 'src/app/shared/post.service';
+import { Post } from '../shared/interfaces';
+import { Subscription } from 'rxjs';
 
 
 @Component({
@@ -6,11 +9,23 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './dashboard-page.component.html',
   styleUrls: ['./dashboard-page.component.scss']
 })
-export class DashboardPageComponent implements OnInit {
+export class DashboardPageComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  posts: Post[];
+  pSub: Subscription;
+  constructor(private postsService: PostService) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.pSub = this.postsService.getAll().subscribe(posts => {
+      this.posts = posts;
+    })
   }
-  
+
+  ngOnDestroy() {
+    if (this.pSub) {
+      this.pSub.unsubscribe();
+    }
+  }
+
+
 }
