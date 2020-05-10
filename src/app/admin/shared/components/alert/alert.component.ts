374da674e73739cc318e9e94107dd2f160927484
Viktor Tiutiun <vvtiutiun@googlemail.com>
2020-05-10 11:14:19 +0300

    Created functionality for subscription and unsubscription of alert service

--- src/app/admin/shared/components/alert/alert.component.ts
+++ src/app/admin/shared/components/alert/alert.component.ts
@@ -1,19 +1,37 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnDestroy } from '@angular/core';
+import { AlertService } from '../../services/alert.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-alert',
   templateUrl: './alert.component.html',
   styleUrls: ['./alert.component.scss']
 })
-export class AlertComponent implements OnInit {
+export class AlertComponent implements OnInit, OnDestroy {
 
   @Input() delay = 5000;
   public text: string;
   public type = 'success';
+  aSub: Subscription;
 
-  constructor() { }
+  constructor(private alertService: AlertService) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+   this.aSub =  this.alertService.alert$.subscribe(alert => {
+      this.text = alert.text;
+      this.type = alert.type;
+
+      //незабываем очистить алерт
+      const timeout = setTimeout(() => {
+        clearTimeout(timeout);
+        this.text = '';
+      }, this.delay)
+    })
   }
 
+  ngOnDestroy() {
+    if (this.aSub) {
+      this.aSub.unsubscribe();
+    }
+  }
 }
